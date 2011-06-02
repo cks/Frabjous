ba0b7b68bf0c5df04d7bfcc98c718d0fa271ce27
Christopher St. John <cks@distributopia.com>
2011-06-02 16:02:10 -0500

    update

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'activerecord-jdbc-adapter'
 gem 'activerecord-jdbcsqlite3-adapter'
 gem 'glassfish'
 
+gem 'koala'
+
 # Bundle gems for the local environment. Make sure to
 # put test-only gems in this group so their generators
 # and rake tasks are available in development mode:
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,8 @@ class HomeController < ApplicationController
   end
 
   def tab
+    var signed_request = params["signed_request"]
+    @oauth = Koala::Facebook::OAuth.new(12345, "67c4c1fae14b74160cb7ef616cccb9e1")
+    @parsed_request = @oauth.parse_signed_request(signed_request)
   end
 end

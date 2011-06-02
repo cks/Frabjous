e08abcc835a3d26aa81335a1bc95c84d7ec7a4d3
Christopher St. John <cks@distributopia.com>
2011-06-01 23:01:03 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frabjous::Application.config.secret_token = 'f9e3663056a62753b76bf6198245b542a1acfb72b879d9963d6ea7fec39801c41abd358dc105c1379cbfeb18182a7631838135985d8a87a91f589c1696fda3df'
